23b0d3363ee6aa5f9d6da1a49dc2e71ca06efbc9
Laith619 <128839039+Laith619@users.noreply.github.com>
2023-07-29 11:03:22 -0700

    using gpt3.5

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def analyse_product_function(product_input: ProductInput):
     ]
 
     response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
+        model="gpt-3.5-turbo-0613",
         messages=messages,
         functions=function_descriptions,
         function_call="auto"
